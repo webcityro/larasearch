c06382b47aa735a7e96b67ac0934d113118904e4
Andrei Valcu <andreivalcu@gmail.com>
2021-10-20 02:36:58 +0300

    Fixed bug into hasFilter method.

--- src/Search/Payloads/MultiFieldsPayload.php
+++ src/Search/Payloads/MultiFieldsPayload.php
@@ -2,25 +2,29 @@
 
 namespace Webcityro\Larasearch\Search\Payloads;
 
-class MultiFieldsPayload extends Payload {
+class MultiFieldsPayload extends Payload
+{
 
 	public $fields;
 
-	public function __construct(array $fields = []) {
+	public function __construct(array $fields = [])
+	{
 		$this->fields = $fields;
 	}
 
-	public function toArray(): array {
+	public function toArray(): array
+	{
 		return [
 			'search' => $this->fields
 		];
 	}
 
-	public function hasFilter(): bool {
+	public function hasFilter(): bool
+	{
 		$found = false;
 
 		foreach ($this->fields as $value) {
-			if (!empty($value)) {
+			if ($value !== '') {
 				$found = true;
 				break;
 			}
